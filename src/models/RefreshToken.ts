344b67864b9fd9881813d52d9e8618ea34d74826
Ted Alan Abakah <doflamingo.ta@gmail.com>
2025-09-04 17:47:54 +0000

    Auth startup

---
+++ src/models/RefreshToken.ts
@@ -0,0 +1,42 @@
+import {
+  Schema,
+  Types,
+  Model,
+  FilterQuery,
+  PaginateOptions,
+  CallbackError,
+  PaginateResult,
+  SchemaOptions,
+  model,
+} from "mongoose";
+import moment from "moment";
+
+export interface RefreshToken {
+  _id: Types.ObjectId;
+  token: string;
+  user_id: string;
+  expires: number;
+  timestamp: number;
+}
+
+const schemaOptions: SchemaOptions = {
+  timestamps: true,
+};
+
+const RefreshTokenSchema = new Schema<RefreshToken>(
+  {
+    token: { type: String, required: true },
+    user_id: { type: String, required: true },
+    expires: { type: Number, default: moment().add(90, 'days').valueOf() },
+    timestamp: { type: Number,  default: moment().valueOf() },
+  },
+  schemaOptions
+);
+
+const RefreshTokenModel: Model<RefreshToken> = model<RefreshToken>(
+  "RefreshToken",
+  RefreshTokenSchema,
+  "Tokens"
+);
+
+export default RefreshTokenModel;
