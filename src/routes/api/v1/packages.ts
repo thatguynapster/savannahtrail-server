85df41358f1b9083bdb18d326e7c0a56715acc0f
Ted Alan-Abakah <35410606+GarrusVk@users.noreply.github.com>
2025-09-26 21:13:32 +0000

    package id route

--- src/routes/api/v1/packages.ts
+++ src/routes/api/v1/packages.ts
@@ -3,11 +3,12 @@ import express from "express";
 const app = express();
 
 import multer from "multer";
-import { getAllPackagesController, paginatePackagesController, createPackageController, updatePackageByIdController, deletePackageByIdController } from "../../../controllers/packages";
+import { getAllPackagesController, paginatePackagesController, createPackageController, updatePackageByIdController, deletePackageByIdController, getPackageByIdController } from "../../../controllers/packages";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.get("/", getAllPackagesController);
+app.get('/:id', getPackageByIdController);
 app.get("/paginate", paginatePackagesController);
 app.post("/create", createPackageController);
 app.put("/:id", updatePackageByIdController);
